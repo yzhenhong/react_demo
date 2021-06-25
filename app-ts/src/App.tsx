00a98479abbbe1375f61fa68b138602dae8340d0
yzhenhong <37287046+yzhenhong@users.noreply.github.com>
2021-06-25 17:24:15 +0800

    项目初始化

---
+++ app-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DynamicForm from './components/dynamic-form'
+
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app-ts/src/components/dynamic-form.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class DynamicForm extends Component {
+    constructor(props:Object) {
+      super(props)
+      this.state = {
+      }
+    };
+  
+    render() {
+      return (
+        <div className="dynamic-form">
+          <div>dynamic-form</div>
+        </div>
+      );
+    };
+  }
